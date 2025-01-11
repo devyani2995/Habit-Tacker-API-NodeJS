aa9f510993341b0c6f219f341623c81d52a9bdca
devyani2995 <devyanisingh42@gmail.com>
2025-01-11 19:01:32 +0530

    change DB_URL

--- src/config/mongooseConfig.js
+++ src/config/mongooseConfig.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 // Retrieve the MongoDB connection URL from environment variables
-const url = process.env.DB_URL
-// const url = process.env.DB_URL_PROD
+// const url = process.env.DB_URL
+const url = process.env.DB_URL_PROD
 
 //Function to establish a connection to MongoDB using Mongoose
 export const connectUsingMongoose = async()=>{
